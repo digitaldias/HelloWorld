6f9376184b6be139a167cd92a590e93ba742e96d
pedias <pedias@microsoft.com>
2016-09-13 13:32:36 +0200

    Construct object graph using IoC
    Added a runtime registry (Structuremap) to do IoC
    that ties it all up in the main program.

---
+++ HelloWorld.Data/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using HelloWorld.Domain.Contracts;
+using System;
+
+namespace HelloWorld.Data
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogException(Exception ex)
+        {
+            Console.WriteLine("EXCEPTION>> " + ex.Message + "\n" + ex.StackTrace.ToString());
+        }
+    }
+}
---
+++ SayHello/DependencyInversion/RuntimeRegistry.cs
@@ -0,0 +1,24 @@
+﻿using HelloWorld.Data;
+using HelloWorld.Domain.Contracts;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello.DependencyInversion
+{
+    public class RuntimeRegistry : Registry
+    {
+        public RuntimeRegistry()
+        {
+            Scan(x => {
+                x.AssembliesFromApplicationBaseDirectory();                
+                x.WithDefaultConventions();
+            });
+
+            For<ILogger>().Singleton().Use<ConsoleLogger>();
+        }
+    }
+}
