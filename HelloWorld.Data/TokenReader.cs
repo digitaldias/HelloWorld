42de4f9a316924d18edaa81ce4ef9f9639f776d7
pedias <pedias@microsoft.com>
2016-09-13 13:21:01 +0200

    TokenReader used to return greeting text
    The TokenReader implementation returns a fixed constant
    as a greeting. Later implementations should change
    this to a greeting repository

---
+++ HelloWorld.Data/TokenReader.cs
@@ -0,0 +1,12 @@
+﻿using HelloWorld.Domain.Contracts.Readers;
+
+namespace HelloWorld.Data
+{
+    public class TokenReader : ITokenReader
+    {
+        public string GetGreeting()
+        {
+            return "The sixth sick shiek's sixth sheep's sick";
+        }
+    }
+}
