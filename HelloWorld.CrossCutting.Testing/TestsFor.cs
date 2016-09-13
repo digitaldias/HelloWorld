c8e70dd43ee63caad1380dfa93f7c9cdb88f24c9
pedias <pedias@microsoft.com>
2016-09-13 08:55:17 +0200

    Create baseclass for unit tests
    In order to be effective in Unit-tests, a base class which does
    automated mocking as well as provide easy access to mocks
    is created.

---
+++ HelloWorld.CrossCutting.Testing/TestsFor.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using StructureMap.AutoMocking.Moq;
+
+namespace HelloWorld.CrossCutting.Testing
+{
+    public class TestsFor<TInstance> where TInstance : class
+    {
+        public MoqAutoMocker<TInstance> AutoMock { get; internal set; }
+
+        public TInstance Instance { get; set; }
+
+
+        public TestsFor()
+        {
+            AutoMock = new MoqAutoMocker<TInstance>();
+
+            Instance = AutoMock.ClassUnderTest;
+        }
+
+
+        public Mock<TContract> GetMockFor<TContract>() where TContract : class
+        {
+            return Mock.Get(AutoMock.Get<TContract>());
+        }
+    }
+}
